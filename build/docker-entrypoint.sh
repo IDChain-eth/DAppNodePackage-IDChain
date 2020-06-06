7710b9822b7a74a3e66ae633ebde489df8c916b9
Michael Bauer <michael@m-bauer.org>
2020-06-06 11:27:31 +0200

    Initial setup for IDChain package

---
+++ build/docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+if [ "$1" = 'idchain' ]; then
+    # Default startup mode
+
+    # Need to init node?
+    if [ ! -f /idchain/initialized ]; then
+        # initialize idchain
+        echo "Initializing IDChain for first startup"
+        geth --datadir /idchain init idchain-genesis.json
+        touch /idchain/initialized
+    fi
+
+    echo "DAppNode config vars:"
+    echo "  SYNCMODE: $SYNCMODE"
+    echo "  EXTRA_OPTS: $EXTRA_OPTS"
+    exec geth --datadir /idchain --networkid 74 --rpc --rpcaddr 0.0.0.0 --rpccorsdomain "*" --rpcvhosts "*" --ws --wsorigins "*" --wsaddr 0.0.0.0 --nousb --syncmode $SYNCMODE $EXTRA_OPTS
+    # exec echo "done!"
+fi
+
+# user provided custom args to container. Just execute whatever was provided
+exec "$@"
